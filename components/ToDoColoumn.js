fa86050a4d35542a3c104ba9b0b371d7f4d888e9
GeoBrodas <geobro2310@gmail.com>
2021-09-24 21:17:12 +0530

    add onDragHandler

--- components/ToDoColoumn.js
+++ components/ToDoColoumn.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 function ToDoColoumn({ items }) {
+  const [tasks, updateTasks] = useState(items);
+
+  function onDragHandler(result) {
+    const items = Array.from(tasks);
+    // make a new copy of the array
+
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    // remove the dragged object from the array and destructure to get hold of the dragged obejct
+
+    items.splice(result.destination.index, 0, reorderedItem);
+    // add the dragged obejct to the array and re-order
+
+    updateTasks(items); // update the new list
+  }
+
   return (
     <div className="m-4 text-center w-44 bg-gray-300 rounded-lg p-2">
       <p className="text-lg font-medium bg-gray-400 rounded-lg">To Do List</p>
       <div>
-        <DragDropContext>
+        <DragDropContext onDragEnd={onDragHandler}>
           <Droppable droppableId="ta">
             {(provided) => (
               <ul
@@ -13,7 +29,7 @@ function ToDoColoumn({ items }) {
                 {...provided.droppableProps}
                 ref={provided.innerRef}
               >
-                {items.map((item, index) => (
+                {tasks.map((item, index) => (
                   <Draggable key={item.id} draggableId={item.id} index={index}>
                     {(provided) => (
                       <li
